f1dffec2bb4d1607d79e03eaae93f3d440c75970
abel encarnacion <encarnacion.abel@gmail.com>
2019-12-04 03:09:02 -0400

    modulo de mantenimiento y menu de navegacion.

---
+++ ProyectoFinal2.0/ProyectoFinal2.0/Controllers/CargoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinal2._0;
+
+namespace ProyectoFinal2._0.Controllers
+{
+    public class CargoController : Controller
+    {
+        private ProyectoFinalEntities db = new ProyectoFinalEntities();
+
+        // GET: Cargo
+        public ActionResult Index()
+        {
+            return View(db.Cargo.ToList());
+        }
+
+        // GET: Cargo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargo cargo = db.Cargo.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // GET: Cargo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cargo/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "cargoId,cargo1")] Cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cargo.Add(cargo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cargo);
+        }
+
+        // GET: Cargo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargo cargo = db.Cargo.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // POST: Cargo/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "cargoId,cargo1")] Cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cargo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cargo);
+        }
+
+        // GET: Cargo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargo cargo = db.Cargo.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // POST: Cargo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cargo cargo = db.Cargo.Find(id);
+            db.Cargo.Remove(cargo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoFinal2.0/ProyectoFinal2.0/Controllers/EmpleadosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinal2._0;
+
+namespace ProyectoFinal2._0.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private ProyectoFinalEntities db = new ProyectoFinalEntities();
+
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            var empleados = db.Empleados.Include(e => e.Cargo).Include(e => e.Departamentos);
+            return View(empleados.ToList());
+        }
+
+        // GET: Empleados/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // GET: Empleados/Create
+        public ActionResult Create()
+        {
+            ViewBag.cargoId = new SelectList(db.Cargo, "cargoId", "cargo1");
+            ViewBag.departamentoId = new SelectList(db.Departamentos, "departamentoId", "nombre");
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "empleadoId,departamentoId,cargoId,emp_codigo,nombre,apellido,telefono,ingreso,salario,estatus")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empleados.Add(empleados);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.cargoId = new SelectList(db.Cargo, "cargoId", "cargo1", empleados.cargoId);
+            ViewBag.departamentoId = new SelectList(db.Departamentos, "departamentoId", "nombre", empleados.departamentoId);
+            return View(empleados);
+        }
+
+        // GET: Empleados/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.cargoId = new SelectList(db.Cargo, "cargoId", "cargo1", empleados.cargoId);
+            ViewBag.departamentoId = new SelectList(db.Departamentos, "departamentoId", "nombre", empleados.departamentoId);
+            return View(empleados);
+        }
+
+        // POST: Empleados/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "empleadoId,departamentoId,cargoId,emp_codigo,nombre,apellido,telefono,ingreso,salario,estatus")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleados).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.cargoId = new SelectList(db.Cargo, "cargoId", "cargo1", empleados.cargoId);
+            ViewBag.departamentoId = new SelectList(db.Departamentos, "departamentoId", "nombre", empleados.departamentoId);
+            return View(empleados);
+        }
+
+        // GET: Empleados/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleados empleados = db.Empleados.Find(id);
+            db.Empleados.Remove(empleados);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
